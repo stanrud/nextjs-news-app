346fd7d2228f840131f53856bb7a8fc90ef3cfeb
Stanislav-Rudnichenko <srudnichenko@gmail.com>
2021-12-20 10:42:23 +0100

    App and document modifications

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,46 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
+import { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+import Nav from 'components/Nav'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { SnackbarProvider } from 'notistack'
+import Container from '@mui/material/Container'
+import theme from 'src/theme'
+import '../styles/globals.css'
 
 export default function MyApp(props) {
-  const { Component, pageProps } = props;
+  const { Component, pageProps } = props
 
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
+      jssStyles.parentElement.removeChild(jssStyles)
     }
-  }, []);
+  }, [])
 
   return (
-    <React.Fragment>
+    <>
       <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <meta name="description" content="Modern News app" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>{pageProps.article?.title}</title>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+        <meta name='description' content='Modern News app' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </React.Fragment>
-  );
+      <SnackbarProvider maxSnack={3} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Nav />
+          <Container maxWidth='md'>
+            <Component {...pageProps} />
+          </Container>
+        </ThemeProvider>
+      </SnackbarProvider>
+    </>
+  )
 }
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
-};
+}
